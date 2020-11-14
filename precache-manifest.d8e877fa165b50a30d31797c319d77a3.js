a6992d0bca4c9b106bef6280c6d72e9d4a87527f
asa121asa <crafta.net@gmail.com>
2020-11-14 19:12:05 +0300

    Updates

---
+++ precache-manifest.d8e877fa165b50a30d31797c319d77a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d6c5ff5f868850e78fa0de9c388258",
+    "url": "/testsComponents/index.html"
+  },
+  {
+    "revision": "5153442ee5dbdea177b5",
+    "url": "/testsComponents/static/css/main.bb8dfbca.chunk.css"
+  },
+  {
+    "revision": "d1a8c297d2de992bc8cd",
+    "url": "/testsComponents/static/js/2.9c0fa19c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/testsComponents/static/js/2.9c0fa19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5153442ee5dbdea177b5",
+    "url": "/testsComponents/static/js/main.47c3af29.chunk.js"
+  },
+  {
+    "revision": "40b0c21d08670c7c2ded",
+    "url": "/testsComponents/static/js/runtime-main.a7a55747.js"
+  }
+]);
