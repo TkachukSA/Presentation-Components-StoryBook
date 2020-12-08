007c4acc35a1ecf79adffe3f2e68c92475bbf873
asa121asa <crafta.net@gmail.com>
2020-12-08 14:41:39 +0300

    redicer ok

---
+++ src/components/accordion/reducer.test.tsx
@@ -0,0 +1,16 @@
+import {reducer, SteteType, togl} from "./reducer";
+import {action} from "@storybook/addon-actions";
+
+
+test('reducer tests', ()=>{
+    const state: SteteType = {
+        collapsed: false
+    }
+
+    let res = reducer(state, {type: togl})
+
+    expect(res).toBe(false)
+
+
+
+})
